c0c1602d0324277bf445a33f0fa050ab9dac757e
jaazpa3 <jaazpa3@gmail.com>
2015-11-22 19:56:36 -0500

    adding my files

--- server.py
+++ server.py
@@ -32,42 +32,6 @@ def group_by (rows,field):
         grouped_rows[value] = rows
     return grouped_rows
 
-
-# Pulls the data from the database file
-# All rows with the given gender
-# (all rows if gender is None)
-# Group the results by cause of death and
-# by year
-
-def pullData (gender):
-    # ignore age & gender for now
-    conn = sqlite3.connect(MORTALITYDB)
-    cur = conn.cursor()
-
-    try: 
-        if gender:
-            cur.execute("""SELECT year, Cause_Recode_39, SUM(1) as total 
-                           FROM mortality
-                           WHERE sex = '%s'
-                           GROUP BY year, Cause_Recode_39""" % (gender))
-        else:
-            cur.execute("""SELECT year, Cause_Recode_39, SUM(1) as total 
-                           FROM mortality
-                           GROUP BY year, Cause_Recode_39""")
-        data = [{"year":int(year),
-                 "cause":int(cause),
-                 "total":total} for (year, cause, total,) in  cur.fetchall()]
-        grouped_data = group_by(data,"cause")
-        for cause in grouped_data:
-            grouped_data[cause]= group_by(grouped_data[cause],"year")
-        conn.close()
-        return grouped_data
-
-    except: 
-        print "ERROR!!!"
-        conn.close()
-        raise
-
 def converttoYearAge(ageKey,ageValue):
     float_age = 0
     if ageKey == u'1':
@@ -139,7 +103,6 @@ def pullbyAvgAge():
         conn.close()
         raise
 
-
 def pullbyAgeAndTopCause():
     conn = sqlite3.connect(MORTALITYDB)
     cur = conn.cursor()
@@ -188,73 +151,6 @@ def pullbyAgeAndTopCause():
         conn.close()
         raise
 
-def activityhelper(): 
-    ### do not touch unless you know what you are doing ###
-    ### creates activity1.json ### 
-    cur.execute("""SELECT mortality.Activity_Code,mortality.Cause_Recode_39, mortality.year, mortality.Age_Key, AVG(mortality.Age_Value), COUNT(mortality.Age_Value), recodecounts.recodetotal
-            FROM mortality
-            JOIN (SELECT Activity_Code, Cause_Recode_39, year, COUNT(Age_Key) as recodetotal
-                  FROM mortality
-                  WHERE Age_Value != '999'
-                  GROUP BY Activity_Code, year, Cause_Recode_39) recodecounts
-            ON mortality.Cause_Recode_39 = recodecounts.Cause_Recode_39 and mortality.year = recodecounts.year
-            WHERE mortality.Age_Value != '999'
-            GROUP BY mortality.Activity_Code,mortality.Cause_Recode_39,
-                mortality.year, 
-                mortality.Age_Key""")
-
-    returnJson = []
-    data = []
-
-    for (activity, cause, year, ageKey, ageValue, refinedAgeCount, recodeCount,) in cur.fetchall():
-        #converts to its real age based on the key and value
-        age = converttoYearAge(ageKey,ageValue)
-
-        data.append({"age": age,
-             "activity": str(activity),
-             "year": str(year), 
-             "cause": str(cause),
-             "refinedAgeCount": refinedAgeCount,
-             "recodeCount": recodeCount})
-
-    #grouping by activity
-    grouped_data = group_by(data, "activity")
-    returnJson.append(grouped_data)
-    conn.close()
-    return returnJson
-    ### do not touch unless you know what you are doing ###   
-
-def pullActivityData():
-    conn = sqlite3.connect(MORTALITYDB)
-    cur = conn.cursor()
-
-    try:
-        #returnJson = activityhelper()
-        returnJson = []
-        with open('./activity1.json') as f:
-            for line in f:
-                returnJson.append(json.loads(line))
-
-        newJson = []
-        groupedData = returnJson[0]["ActivityData"][0] #artifact
-
-        for activity in groupedData.keys():
-            activityJson = {"activity": activity}
-            for row in groupedData[activity]:
-                if row["cause"] in activityJson:
-                    activityJson[row["cause"]] += float(row["refinedAgeCount"]) #not optimized
-                else:
-                    activityJson[row["cause"]] = float(row["refinedAgeCount"])
-            newJson.append(activityJson)
-        newest = group_by(newJson,"activity")
-
-        return newest
-
-    except:
-        print "ERROR!!!"
-        conn.close()
-        raise
-
 def newDataPull():
     conn = sqlite3.connect(MORTALITYDB)
     cur = conn.cursor()
@@ -284,33 +180,6 @@ def newDataPull():
         print "ERROR!!!"
         conn.close()
         raise
-
-
-                         
-# URI for getting data
-# pass a gender argument as:
-#    data?gender=M  
-#    data?gender=F
-#    data?gender=
-#
-# Return the result in JSON format
-
-@get("/data")
-def data ():
-    print list(request.query)
-    gender = request.query.gender
-
-    print "gender =", gender
-    return pullData(gender)
-
-    
-# URI for getting a static file from the
-# server
-# For instance, mortality-demo.html
-
-@get('/demo.html')
-def static (name="demo.html"):
-    return static_file(name, root='.')
 
 @get('/test.html')
 def static (name="test.html"):
